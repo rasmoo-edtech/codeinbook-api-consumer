9e35af3ccd0f8d8af199c9fa39a177c7ecde6617
Felipe Alves <felipe.barbosa@ext.valid.com>
2024-10-03 16:29:05 -0300

    Change consumer groups

--- src/main/java/com/rasmoo/codeinbook/application/adapter/in/listener/PaymentListener.java
+++ src/main/java/com/rasmoo/codeinbook/application/adapter/in/listener/PaymentListener.java
@@ -3,17 +3,20 @@ package com.rasmoo.codeinbook.application.adapter.in.listener;
 import com.rasmoo.codeinbook.common.dto.PaymentDTO;
 import com.rasmoo.codeinbook.domain.port.in.PaymentServicePort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentListener {
 
     private final PaymentServicePort paymentServicePort;
 
-    @KafkaListener(topics = "payment-topic", groupId = "group")
+    @KafkaListener(topics = "payment-topic", groupId = "group-payment")
     public void paymentTopicEvent(PaymentDTO message) {
-        paymentServicePort.process(message);
+        log.info("consumer 1 - id {}", message.id() );
+   //     paymentServicePort.process(message);
     }
 }
